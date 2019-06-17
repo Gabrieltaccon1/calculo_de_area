8be3f608c1e70fcb8f690803344c6138f94246bf
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-17 16:10:39 -0300

    area de um terreno

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace area_de_um_terreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, preco, PrecoMetroQuadrado, area;
+
+            Console.WriteLine("DIGITE O VALOR REFERENTE A LARGURA");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("DIGITE O COMPRIMENTO");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("QUAL VALOR DO METRO QUADRADO");
+            PrecoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * PrecoMetroQuadrado;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO= " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+
+        }
+    }
+}
